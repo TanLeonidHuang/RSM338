28116a2cfa16e87758213bbf1f48f4262a37513b
huangn12 <nathaniel.huang@mail.utoronto.ca>
2022-09-16 12:47:41 -0400

    Q1

--- Assignment1.py
+++ Assignment1.py
@@ -2,3 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+
+data = pd.read_csv(r'C:/Users/natha/OneDrive/Desktop/Coding/RSM338/RSM338-Assignment-1/data/AAPL.csv')
+df = pd.DataFrame(data)
+get_rows_100 = df.head(100)
+
+print(get_rows_100)
